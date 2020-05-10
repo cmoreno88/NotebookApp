fd55637f46ef6d7114b604d610ab748faf40ed36
cmoreno88 <cmore035@ucr.edu>
2020-05-10 13:32:06 -0700

    functionallity?
    Need to actualy implement functionality into key feature classes:
    textualmessage, messagelist, and image

--- Notebook.cs
+++ Notebook.cs
@@ -10,13 +10,13 @@ namespace NotebookApp
     {
         //notification messages
         public const string IntroMessage = "Welcome to notebook program";
-        public const string OuttrMessage = "Hope you'll use the notebook again, GoodBye!";
+        public const string OuttroMessage = "Hope you'll use the notebook again, GoodBye!";
 
         //list of pages
         List<IPageable> pages = new List<IPageable>();
-
         public delegate void SimpleFunction(string command);
         private Dictionary<string, SimpleFunction> commandLineArgs = new Dictionary<string, SimpleFunction>();
+        
         //these are our keys
         public readonly string show = "show", _new = "new", delete = "delete";
 
@@ -65,7 +65,7 @@ namespace NotebookApp
                         break;
 
                     //delete
-                    case 3:
+                    case 2:
                         commandLineArgs.Remove(delete);
                         commandLineArgs.Add(delete = commandLineKeywords[i], Delete);
                         break;
@@ -74,19 +74,75 @@ namespace NotebookApp
             }
         }
 
-        public void Show(string command)
+        //property methods, set to private because they are called by the accessor
+        private void New(string command)
         {
-
+            Console.WriteLine("New method " + command);
+            switch (command)
+            {
+                case "":
+                    Console.WriteLine("New commands:");
+                    Console.WriteLine("message  create new message page");
+                    Console.WriteLine("list     create new list page");
+                    Console.WriteLine("image    create new image page");
+                    break;
+                case "message":
+                    Console.WriteLine("Creating message page!");
+                    break;
+                case "list":
+                    Console.WriteLine("Creating list page!");
+                    break;
+                case "image":
+                    Console.WriteLine("Creating image page!");
+                    break;
+            }
         }
 
-        public void New(string command)
+        private void Show(string command)
         {
-
+            Console.WriteLine("Show method " + command);
+            switch (command)
+            {
+                case "":
+                    Console.WriteLine("Show commands:");
+                    Console.WriteLine("pages          lists all created pages");
+                    Console.WriteLine("id of page    display that page");
+                    break;
+                case "pages":
+                    Console.WriteLine("Showing all pages!");
+                    break;
+                default:
+                    int number;
+                    //if the second value is an integer then it will be parsed
+                    if(int.TryParse(command, out number))
+                    {
+                        Console.WriteLine("Showing page " + number);
+                    }
+                    break;
+            }
         }
 
-        public void Delete(string command)
+        private void Delete(string command)
         {
-
+            switch (command)
+            {
+                case "":
+                    Console.WriteLine("Delete commands:");
+                    Console.WriteLine("all          delete all created pages");
+                    Console.WriteLine("id of page   delete that page");
+                    break;
+                case "all":
+                    Console.WriteLine("Deleting all pages!");
+                    break;
+                default:
+                    int number;
+                    //if the second value is an integer then it will be parsed
+                    if (int.TryParse(command, out number))
+                    {
+                        Console.WriteLine("Deleting page " + number);
+                    }
+                    break;
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,6 +10,40 @@ namespace NotebookApp
     {
         static void Main(string[] args)
         {
+            Notebook notebook = new Notebook();     //"see", "create", "remove"
+            const string ExitProgramKeyword = "exit";
+            string commandPrompt = "Please enter " + notebook.show + ", " + notebook.delete + ", or " + notebook._new;
+
+            Console.WriteLine(Notebook.IntroMessage);
+            Console.WriteLine(commandPrompt);
+            string input = "";
+
+            do
+            {
+                input = Console.ReadLine();
+
+                //breaks input up into tokens in an array
+                string[] commands = input.Split();
+
+
+                try {
+                    /*get the first command...show, new, or delete
+                    and pass the second command to the functions
+                    still needs to be in the correct format*/
+                    notebook[commands[0]]((commands.Length > 1) ? commands[1] : "");
+                }
+                //Stops the program is ExitProgramKeyword is entered
+                catch (KeyNotFoundException)
+                {
+                    if (input != ExitProgramKeyword)
+                    {
+                        Console.WriteLine(commandPrompt);
+                    }
+                }
+            } while (input != ExitProgramKeyword);
+
+            Console.WriteLine(Notebook.OuttroMessage);
+
         }
     }
 }
